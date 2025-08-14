8eb4ae49beab455dab783e310dfd508cd4ce29d3
Shamil <shamilpp4115@gmail.com>
2025-08-14 16:00:06 +0530

    fixed firebase package issue

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -49,7 +49,7 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyDp_cX1KLeV7ACn1EEJyTEuDXBAAFerAig',
-    appId: '1:388401607033:android:c2a5879d298559d9787fa8',
+    appId: '1:388401607033:android:2ff59142411c9ea9787fa8',
     messagingSenderId: '388401607033',
     projectId: 'kleanit-c4f88',
     storageBucket: 'kleanit-c4f88.firebasestorage.app',
@@ -57,12 +57,12 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyAbulKlglQTjQvm0yRvtnzcC7_b4E3rX4A',
-    appId: '1:388401607033:ios:7f1babdcd7080793787fa8',
+    appId: '1:388401607033:ios:bda37b8339e40a56787fa8',
     messagingSenderId: '388401607033',
     projectId: 'kleanit-c4f88',
     storageBucket: 'kleanit-c4f88.firebasestorage.app',
     androidClientId: '388401607033-1u0lfilr31rb1d4d1hmasdlstfja80gs.apps.googleusercontent.com',
-    iosClientId: '388401607033-an0ebkia066pghvbeqk83qnsv8luhu1d.apps.googleusercontent.com',
+    iosClientId: '388401607033-i8ue8resaje112nor3019br5vgt5kqar.apps.googleusercontent.com',
     iosBundleId: 'com.spydo.kleanitcustomer',
   );
 
